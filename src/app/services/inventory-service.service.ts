33a3ee50bbbdcb0def247641ea73a3dea13ebfdf
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-07-29 22:06:15 -0400

    fffff

--- src/app/services/inventory-service.service.ts
+++ src/app/services/inventory-service.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class IngredientServiceService {
+export class InventoryServiceService {
 
   constructor() { }
 }
