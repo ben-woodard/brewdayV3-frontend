9113a8b57f7f3331bf657df5b5356df7667d7778
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-07-29 22:05:31 -0400

    adding to inventory

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class InventoryComponent implements OnInit{
 
   constructor() {
-    
+
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    
   }
 
 }
