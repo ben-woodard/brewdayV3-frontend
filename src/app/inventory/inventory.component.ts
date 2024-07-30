94d723e329a58dbe6d9d30214819dd745251b692
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-07-29 21:59:24 -0400

    Update inventory.component.ts

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
-export class InventoryComponent {
+export class InventoryComponent implements OnInit{
+
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
