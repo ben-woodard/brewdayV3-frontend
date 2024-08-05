ff053da866eaa8a3a6e87bc15bc8dfa4cf20a797
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-08-05 13:51:35 -0400

    progress

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/User';
+import { AdminServiceService } from '../services/admin-service.service';
+import { UserServiceService } from '../services/user-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit{
+  user: User | null = null
+  userIsAdmin: boolean = false
+
+  constructor(
+    private adminService: AdminServiceService,
+    private userService: UserServiceService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.userObservable.subscribe(user => this.user = user);
+    if(this.user && this.user?.authorities?.some(auth => auth.authority === 'ROLE_ADMIN')) {
+      this.userIsAdmin = true;
+      this.adminService.getAllUsers().subscribe(
+        response => {
+          console.log(response)
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    } else {
+      this.router.navigate(['/'])
+    }
+  }
 
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -21,6 +21,7 @@ export class NavBarComponent implements OnInit {
       this.userIsAdmin = true;
     }
 
+    
 
 
   }
